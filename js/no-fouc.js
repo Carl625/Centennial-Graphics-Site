a535ecc720a347e3a7550cc1ded6dd9b689633c8
David Aylaian <DavidAylaian@users.noreply.github.com>
2016-09-30 19:20:39 -0400

    Added comments

--- js/no-fouc.js
+++ js/no-fouc.js
@@ -1,7 +1,11 @@
-// Prevent FOUC (flash of unstyled content) - http://johnpolacek.com/2012/10/03/help-prevent-fouc/
+// Prevent FOUC (flash of unstyled content) - based on http://johnpolacek.com/2012/10/03/help-prevent-fouc/
+
+// hide webpage
 document.documentElement.className = 'no-fouc';
 
-$(window).load(function() {
-	$('.no-fouc').removeClass('no-fouc');
-	console.log("Page loaded");
+// called when page is completely finished loading
+$(window).load(function()
+{
+	$('.no-fouc').removeClass('no-fouc');	// show webpage
+	console.log("Page loaded");				// for debugging
 });
