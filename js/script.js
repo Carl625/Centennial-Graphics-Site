48c41513ae291ce8d60b5244f98a0c4c6d5bfe70
David Aylaian <DavidAylaian@users.noreply.github.com>
2016-10-03 22:21:55 -0400

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// called when page finishes loading
-$(document).ready(function() {
-	// mobile navbar
-	$(".button-collapse").sideNav();
-});
-    $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
